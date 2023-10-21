51f0806d2bbb9a64dc063e41d55cb81ec3d5ea05
Rey-Diaz <Reinoldiaz@gmail.com>
2023-10-21 11:50:01 -0400

    test

--- gptweet/src/app.py
+++ gptweet/src/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify
 import sys
-sys.path.append('./src/utilities')
 import util
-from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -11,6 +9,6 @@ app = Flask(__name__)
 def run_util():
     result = util.some_function()  # Call the function you want to run from util.py
     return jsonify({"result": result})
-    CORS(app)
+
 if __name__ == '__main__':
     app.run(port=5000)
