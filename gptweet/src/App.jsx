42bb2c464bb3cf63e58c98c2279c4b5087e68c22
Rey-Diaz <Reinoldiaz@gmail.com>
2023-10-21 13:07:54 -0400

    Update App.jsx

--- gptweet/src/App.jsx
+++ gptweet/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyComponent from './components/myconponent'
+import RunButton from './components/runButton'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,7 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <MyComponent />
+        <RunButton />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
