dcd0f3d887476f7646526ef4b71d7d031d965375
Rey-Diaz <Reinoldiaz@gmail.com>
2023-10-30 12:04:29 -0400

    tw

--- gptweet/src/App.jsx
+++ gptweet/src/App.jsx
@@ -1,19 +1,28 @@
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Core/Footer';
 import Home from './pages/Home';
-import './App.css';
 
 function App() {
-
   return (
-    <div className="app-container">
-      <NavBar />
-  
-        <main className="main-content">
-          <Home />
-        </main>
-      
-      <Footer />
+    // Flex container with vertical direction
+    <div className="flex flex-col min-h-screen">
+      {/* Fixed NavBar at the top of the screen */}
+      <div className="fixed top-0 w-full z-10">
+        <NavBar />
+      </div>
+
+      {/* Main content with padding top to offset the fixed NavBar */}
+      {/* The pt-* class should be adjusted based on the actual height of your NavBar */}
+      {/* pb-* class should be adjusted based on the actual height of your Footer */}
+      <main className="flex-grow pt-16 pb-14">
+        <Home />
+      </main>
+
+      {/* Footer at the bottom */}
+      {/* Fixed at the bottom of the viewport */}
+      <div className="fixed bottom-0 w-full">
+        <Footer />
+      </div>
     </div>
   );
 }
--- gptweet/src/pages/Home.jsx
+++ gptweet/src/pages/Home.jsx
@@ -1,30 +1,28 @@
-
 import Carousel from '../components/Carousel/Carousel'; // Import the Carousel component
 import RunButton from '../components/runButton/runButton'; // Import the RunButton component
-import './Home.css'; // Import the CSS for styling
 
 function Home() {
   return (
-    <div className="home-container">
-      <header className="landing-header">
-        <h1>Welcome to GPTweet</h1>
-        <p>Your go-to platform for AI-generated tweets</p>
+    <div className="container mx-auto px-4">
+      <header className="text-center py-10">
+        <h1 className="text-4xl font-bold text-gray-800">Welcome to GPTweet</h1>
+        <p className="text-gray-600 mt-3">Your go-to platform for AI-generated tweets</p>
       </header>
-      <main className="landing-content">
-        <section className="carousel-section">
-          <h2>Discover Amazing AI-Generated Tweets</h2>
+      <main>
+        <section className="my-8">
+          <h2 className="text-2xl font-semibold text-gray-800">Discover Amazing AI-Generated Tweets</h2>
           <Carousel />
         </section>
-        <section className="action-section">
-          <h2>Get Started Now</h2>
-          <p>Ready to experience the power of AI-generated tweets?</p>
-          <div className="run-button-container">
+        <section className="my-8">
+          <h2 className="text-2xl font-semibold text-gray-800">Get Started Now</h2>
+          <p className="text-gray-600 mt-3">Ready to experience the power of AI-generated tweets?</p>
+          <div className="flex justify-center mt-6">
             <RunButton />
           </div>
         </section>
       </main>
-      <footer className="landing-footer">
-        <p>&copy; {new Date().getFullYear()} GPTweet. All rights reserved.</p>
+      <footer className="text-center py-6">
+        <p className="text-gray-600">&copy; {new Date().getFullYear()} GPTweet. All rights reserved.</p>
       </footer>
     </div>
   );
