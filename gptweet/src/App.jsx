3f635241d7ed07d692bd8893dccbaadaca1bc07f
Rey-Diaz <Reinoldiaz@gmail.com>
2023-10-30 15:59:20 -0400

    sdasdasd

--- gptweet/src/App.jsx
+++ gptweet/src/App.jsx
@@ -3,29 +3,35 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import './App.css';
+import Carousel from './components/Carousel';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
-  // Effect to toggle the class on the body element
+  // Effect to toggle the class on the body element and set background color
   useEffect(() => {
+    // Set the background color directly on the body element
+    const backgroundColor = darkMode ? '#121212' : '#ffffff'; // Use your dark and light background colors
+    document.body.style.backgroundColor = backgroundColor;
+
+    // Toggle the dark-mode class for content colors
     if (darkMode) {
       document.body.classList.add('dark-mode');
     } else {
       document.body.classList.remove('dark-mode');
     }
-  }, [darkMode]); // Depend on darkMode state
+  }, [darkMode]); // This effect runs when darkMode state changes
 
   const handleThemeToggle = () => {
-    setDarkMode(!darkMode);
+    setDarkMode(!darkMode); // Toggle the darkMode state
   };
 
-  // Removed themeClass as we are now toggling class on the body element
   return (
     <div className="flex flex-col min-h-screen">
       <Header onThemeToggle={handleThemeToggle} darkMode={darkMode} />
       <main className="flex-grow">
         <Home />
+        <Carousel />
       </main>
       <Footer />
     </div>
--- gptweet/src/components/Footer.jsx
+++ gptweet/src/components/Footer.jsx
@@ -1,13 +1,83 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import { useTheme } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Footer = () => {
+  const theme = useTheme();
+
   return (
-    <Box component="footer" className="fixed inset-x-0 bottom-0 bg-gray-800 text-white">
-      <Container maxWidth="lg" className="py-4">
-        <Typography variant="body1" className="text-center">
-          &copy; {new Date().getFullYear()} My React App
+    <Box
+      component="footer"
+      sx={{
+        bgcolor: 'background.paper',
+        color: 'text.secondary',
+        pt: 3,
+        pb: 3,
+        mt: 'auto',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Company
+            </Typography>
+            <List>
+              <ListItem>
+                <Link href="#" color="inherit">About Us</Link>
+              </ListItem>
+              <ListItem>
+                <Link href="#" color="inherit">Careers</Link>
+              </ListItem>
+              <ListItem>
+                <Link href="#" color="inherit">Press</Link>
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Resources
+            </Typography>
+            <List>
+              <ListItem>
+                <Link href="#" color="inherit">Blog</Link>
+              </ListItem>
+              <ListItem>
+                <Link href="#" color="inherit">Help Center</Link>
+              </ListItem>
+              <ListItem>
+                <Link href="#" color="inherit">Contact Us</Link>
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Us
+            </Typography>
+            <IconButton aria-label="Facebook" color="inherit">
+              <FacebookIcon />
+            </IconButton>
+            <IconButton aria-label="Twitter" color="inherit">
+              <TwitterIcon />
+            </IconButton>
+            <IconButton aria-label="Instagram" color="inherit">
+              <InstagramIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Divider sx={{ my: 3 }} />
+        <Typography variant="body2" color="text.secondary" align="center">
+          &copy; {new Date().getFullYear()} My React App. All rights reserved.
         </Typography>
       </Container>
     </Box>
